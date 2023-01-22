917bbe7732f06743120e613548cdbdff6458305d
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-01-21 22:48:31 -0300

    ADD Entities Pessoa and Endereco

--- src/main/java/com/attornatus/backend/entity/Endereco.java
+++ src/main/java/com/attornatus/backend/entity/Endereco.java
@@ -1,4 +1,26 @@
 package com.attornatus.backend.entity;
 
+import lombok.*;
+import javax.persistence.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String logradouro;
+    @Column(nullable = false)
+    private String cep;
+    @Column(nullable = false)
+    private String numero;
+    @Column(nullable = false)
+    private String cidade;
+
 }
--- src/main/java/com/attornatus/backend/entity/Pessoa.java
+++ src/main/java/com/attornatus/backend/entity/Pessoa.java
@@ -1,4 +1,52 @@
 package com.attornatus.backend.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @OneToMany
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_endereco")
+    private Endereco enderecoPrincipal;
+
+    @Column(nullable = false, unique = true)
+    private String numeroCadastro;
+
+    public void setNumeroCadastro() {
+        this.numeroCadastro = UUID.randomUUID().toString();
+    }
+
+    public void setEnderecoPrincipal(Endereco endereco){
+        this.enderecoPrincipal = endereco;
+        if (!enderecos.contains(endereco)) {
+            enderecos.add(endereco);
+        }
+    }
+
+
 }
