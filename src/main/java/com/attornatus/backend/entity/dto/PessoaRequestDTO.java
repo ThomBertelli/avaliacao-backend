37d1ecf8d066f7a4383d0ccd1e6de0890d984a0d
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-01-21 22:53:06 -0300

    ADD Entities DTOs Pessoa and Endereco

---
+++ src/main/java/com/attornatus/backend/entity/dto/PessoaRequestDTO.java
@@ -0,0 +1,35 @@
+package com.attornatus.backend.entity.dto;
+
+import com.attornatus.backend.entity.Endereco;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PessoaRequestDTO {
+
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private Date dataNascimento;
+
+    @NotBlank
+    private List<Endereco> enderecos = new ArrayList<>();
+    @NotBlank
+    private Endereco enderecoPrincipal;
+
+    @NotBlank
+    private String numeroCadastro;
+
+}
